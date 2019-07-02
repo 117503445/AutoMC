b0f4a554fcd4237fd281f5520ab79af24b970c7a
117503445 <t117503445@gmail.com>
2019-07-02 20:14:29 +0800

    0702-2014

--- AutoMc/Action.cs
+++ AutoMc/Action.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Threading;
 using TLib.Windows;
 using WF = System.Windows.Forms;
 namespace AutoMc
@@ -12,32 +15,24 @@ namespace AutoMc
     /// <summary>
     /// 表示一个按键动作 (一直按左键,一直按w键)
     /// </summary>
-    public class KeyAction : IDisposable
+    public sealed class KeyAction : IDisposable
     {
         /// <summary>
-        /// 标识按键 如 Ctrl Shift,支持 ModifierKeys.Ctrl | ModifierKeys.Shift
-        /// </summary>
-        public ModifierKeys ModifierKeys { get; set; }
-        /// <summary>
-        /// 普通键盘按键,如 C
-        /// </summary>
-        public Key Key { get; set; }
-        /// <summary>
-        /// 描述
+        /// 对这次按键动作的文本描述
         /// </summary>
         public string Describe { get; set; }
         /// <summary>
         /// 按下热键后 要一直按住的键盘键值
         /// </summary>
-        public List<Key> PressedKey { get; set; }
+        private List<Key> PressedKey { get; }
         /// <summary>
         /// 按下热键后 要一直按住的鼠标键值
         /// </summary>
-        public List<MouseButton> PressedMouse { get; set; }
+        private List<MouseButton> PressedMouse { get; }
+        /// <summary>
+        /// 返回当前热键是否已经成功注册
+        /// </summary>
         [Newtonsoft.Json.JsonIgnore()]
-        private readonly HotKey hotKey;
-        [Newtonsoft.Json.JsonIgnore()]
-
         public bool IsKeyRegistered
         {
             get
@@ -46,16 +41,39 @@ namespace AutoMc
                 {
                     return hotKey.IsKeyRegistered;
                 }
-                catch (Exception)
+                catch (NullReferenceException)
                 {
 
                     return false;
                 }
             }
         }
-        public KeyAction(ModifierKeys modifierKeys, WF.Keys key, Window window, string describe, List<Key> pressedKey, List<MouseButton> pressedMouse)
+        /// <summary>
+        /// 私有热键对象
+        /// </summary>
+        [Newtonsoft.Json.JsonIgnore()]
+        private readonly HotKey hotKey;
+        /// <summary>
+        /// 用于模拟一直按下按键
+        /// </summary>
+        [Newtonsoft.Json.JsonIgnore()]
+        private readonly DispatcherTimer timer = new DispatcherTimer()
         {
-            hotKey = new HotKey(modifierKeys, key, window);
+            Interval = TimeSpan.FromMilliseconds(10),
+            IsEnabled = false,
+        };
+        /// <summary>
+        /// 表示一个按键动作 (一直按左键,一直按w键)
+        /// </summary>
+        /// <param name="hotkeyModifierKeys">标识按键 如 Ctrl Shift,支持 ModifierKeys.Ctrl | ModifierKeys.Shift</param>
+        /// <param name="hotkeyNormalKeys">普通键盘按键,如 C</param>
+        /// <param name="window">传入this即可</param>
+        /// <param name="describe">对这次按键动作的文本描述</param>
+        /// <param name="pressedKey">按下热键后 要一直按住的键盘键值</param>
+        /// <param name="pressedMouse">按下热键后 要一直按住的鼠标键值</param>
+        public KeyAction(ModifierKeys hotkeyModifierKeys, WF.Keys hotkeyNormalKeys, Window window, string describe, List<Key> pressedKey, List<MouseButton> pressedMouse)
+        {
+            hotKey = new HotKey(hotkeyModifierKeys, hotkeyNormalKeys, window);
             hotKey.HotKeyPressed += HotKey_HotKeyPressed;
             Describe = describe;
             if (pressedKey == null)
@@ -68,20 +86,49 @@ namespace AutoMc
             }
             PressedKey = pressedKey;
             PressedMouse = pressedMouse;
-        }
 
-        private void HotKey_HotKeyPressed(HotKey obj)
-        {
+
             foreach (var key in PressedKey)
             {
-                KeyboardSimulation.Press(key);
+                KeyboardSimulation.Reset();
+                timer.Tag = key;
             }
             foreach (var mouse in PressedMouse)
             {
                 MouseSimulation.Down(mouse);
             }
-        }
 
+
+            timer.Tick += Timer_Tick;
+        }
+        /// <summary>
+        /// 由 hotkeyArgs 定义的热键触发,持续按下 keyPressedArgs 定义的键盘和鼠标键值
+        /// </summary>
+        /// <param name="hotkeyArgs"></param>
+        /// <param name="describe"></param>
+        /// <param name="keyPressedArgs"></param>
+        public KeyAction(HotkeyArgs hotkeyArgs, string describe, KeyPressedArgs keyPressedArgs) : this(HotkeyArgs.PassThroughNonNull(hotkeyArgs).HotkeyModifierKeys, hotkeyArgs.HotkeyNormalKey, hotkeyArgs.Window, describe, KeyPressedArgs.PassThroughNonNull(keyPressedArgs).PressedKey, keyPressedArgs.PressedMouse)
+        {
+            Contract.Requires(hotkeyArgs != null);
+            Contract.Requires(keyPressedArgs != null);
+        }
+        private void HotKey_HotKeyPressed(HotKey obj)
+        {
+            if (timer.IsEnabled)
+            {
+                KeyboardSimulation.Reset();
+                foreach (var key in PressedMouse)
+                {
+                    MouseSimulation.Up(key);
+                }
+            }
+            timer.IsEnabled = !timer.IsEnabled;
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            var key = (Key)((DispatcherTimer)sender).Tag;
+            KeyboardSimulation.Press(key);
+        }
         public void Dispose()
         {
             try
@@ -89,11 +136,40 @@ namespace AutoMc
                 hotKey.UnregisterHotKey();
                 hotKey.Dispose();
             }
-            catch (Exception)
+            catch (NullReferenceException)
             {
 
 
             }
+        }
+    }
+    public class HotkeyArgs
+    {
+        public ModifierKeys HotkeyModifierKeys { get; set; }
+        public WF.Keys HotkeyNormalKey { get; set; }
+        public Window Window { get; set; }
+        public static HotkeyArgs PassThroughNonNull(HotkeyArgs hotkeyArgs)
+        {
+            if (hotkeyArgs == null)
+            {
+                throw new ArgumentNullException(nameof(hotkeyArgs));
+            }
+            return hotkeyArgs;
+        }
+    }
+    public class KeyPressedArgs
+    {
+#pragma warning disable CA2227 // Collection properties should be read only
+        public List<Key> PressedKey { get; set; }
+        public List<MouseButton> PressedMouse { get; set; }
+#pragma warning restore CA2227 // Collection properties should be read only
+        public static KeyPressedArgs PassThroughNonNull(KeyPressedArgs keyPressedArgs)
+        {
+            if (keyPressedArgs == null)
+            {
+                throw new ArgumentNullException(nameof(keyPressedArgs));
+            }
+            return keyPressedArgs;
         }
     }
 }
--- AutoMc/MainWindow.xaml.cs
+++ AutoMc/MainWindow.xaml.cs
@@ -21,79 +21,32 @@ namespace AutoMc
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public sealed partial class MainWindow : Window, IDisposable
     {
+        List<KeyAction> keyActions=new List<KeyAction>();
         public MainWindow()
         {
             InitializeComponent();
-            KeyAction keyAction = new KeyAction(ModifierKeys.Alt, System.Windows.Forms.Keys.P, this,"",new List<Key>(),new List<MouseButton>());
+            var keyAction = new KeyAction(ModifierKeys.Control, System.Windows.Forms.Keys.P, this, "de", new List<Key>() { Key.W }, new List<MouseButton>() {MouseButton.Left});
             string json = JsonConvert.SerializeObject(keyAction);
             Console.WriteLine(json);
+            keyActions.Add(keyAction);
+
         }
-        [DllImport("user32.dll")]
-        public static extern int SetWindowLong(IntPtr hWnd, int nIndex, IntPtr dwNewLong);
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            HotKey hotkey_upleft = new HotKey(ModifierKeys.Alt | ModifierKeys.Shift, System.Windows.Forms.Keys.E, this);
-            hotkey_upleft.HotKeyPressed += HotKey_HotKeyPressed;
-            HotKey hotkey_up = new HotKey(ModifierKeys.Alt, System.Windows.Forms.Keys.E, this);
-            hotkey_up.HotKeyPressed += Key_HotKeyPressed;
-            HotKey hotkey_left = new HotKey(ModifierKeys.Shift, System.Windows.Forms.Keys.E, this);
-            hotkey_left.HotKeyPressed += Hot_HotKeyPressed;
-            HotKey hotKey_right = new HotKey(ModifierKeys.Alt, System.Windows.Forms.Keys.W, this);
-            hotKey_right.HotKeyPressed += HotKey_right_HotKeyPressed;
-
-            #region 让当前窗体不获取输入焦点
-            //WindowInteropHelper wndHelper = new WindowInteropHelper(this);
-            //IntPtr HWND = wndHelper.Handle;
-            //int GWL_EXSTYLE = -20;
-            //SetWindowLong(HWND, GWL_EXSTYLE, (IntPtr)(0x8000000));
-            #endregion
-
+            //keyAction.Dispose();
         }
 
-        private void HotKey_right_HotKeyPressed(HotKey obj)
-        {
-            PostMessage(Handle, 0x204, 0, 1);
-        }
 
-        [DllImport("User32.dll", EntryPoint = "FindWindow")]
-        public static extern IntPtr FindWindow(string ClassN, string WindN);
-        private void Hot_HotKeyPressed(HotKey obj)
-        {
-            //IntPtr intPtr = (IntPtr)int.Parse(TbIntPtr.Text, System.Globalization.NumberStyles.HexNumber);
-            //PostMessage(Handle, 0x0100, 0x57, 1);
-            MouseSimulation.Down(MouseButton.Left);
-        }
-
-        private void Key_HotKeyPressed(HotKey obj)
-        {
-            //IntPtr intPtr = (IntPtr)int.Parse(TbIntPtr.Text, System.Globalization.NumberStyles.HexNumber);
-            PostMessage(Handle, 0x201, 0, 1);
-        }
-        public IntPtr Handle
-        {
-            get
-            {
-                return FindWindow(null, "Minecraft 1.12.2");
-            }
-        }
-        [DllImport("user32.dll", EntryPoint = "PostMessage")]
-        public static extern IntPtr PostMessage(IntPtr hWnd, uint Msg, int wParam, int lParam);
-        private void HotKey_HotKeyPressed(HotKey obj)
-        {
-            //IntPtr intPtr = (IntPtr)int.Parse(TbIntPtr.Text, System.Globalization.NumberStyles.HexNumber);
-
-            PostMessage(Handle, 0x0100, 0x57, 1);
-            PostMessage(Handle, 0x201, 0, 1);
-            //Console.WriteLine(1);
-            //await Task.Delay(1000);
-            //MouseSimulation.Down(MouseButton.Left);
-            //KeyboardSimulation.Press(Key.W);
-        }
         private void CboIsTopMost_Click(object sender, RoutedEventArgs e)
         {
             Topmost = (bool)CboIsTopMost.IsChecked;
+        }
+
+        public void Dispose()
+        {
+            //keyAction?.Dispose();
         }
     }
 }
